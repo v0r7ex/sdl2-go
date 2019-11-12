de1acc88ff5aacf0733952188b5237fb7c495afb
Violeta <violetadruga@gmail.com>
2019-11-11 21:08:59 -0800

    lesson 2

--- lesson_02.go
+++ lesson_02.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/veandco/go-sdl2/sdl"
+)
 
+var running = true
+
+func main() {
+	// Initialize SDL
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		fmt.Printf("Could not initialize SDL2: %s\n", sdl.GetError())
+		return
+	}
+	defer sdl.Quit()
+
+	// Create the window
+	window, err := sdl.CreateWindow("SDL2", sdl.WINDOWPOS_CENTERED, sdl.WINDOWPOS_CENTERED, 1024, 576, sdl.WINDOW_RESIZABLE)
+	if err != nil {
+		fmt.Printf("Could not create window: %s\n", sdl.GetError())
+	}
+	defer window.Destroy()
+
+	// Run game loop
+	for running {
+		getEvents()
+	}
+}
+
+func getEvents() {
+	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+		switch e := event.(type) {
+		case *sdl.QuitEvent:
+			running = false
+			break
+		case *sdl.KeyboardEvent:
+			if e.Keysym.Sym == sdl.K_ESCAPE{
+				running = false
+			}
+			break
+		}
+	}
 }
