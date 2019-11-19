fe7f70a6b1650a83d2c18f5aae05436bb4f8a444
Violeta <violetadruga@gmail.com>
2019-11-18 21:46:24 -0800

    lesson 3

---
+++ lesson_03.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+type Game struct {
+	running bool
+	score int
+	colored bool
+}
+
+func main() {
+	game := Game {running: true}
+
+	// Initialize SDL
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		fmt.Printf("Could not initialize SDL2: %s\n", sdl.GetError())
+		return
+	}
+	defer sdl.Quit()
+
+	// Create the window
+	window, err := sdl.CreateWindow("SDL2", sdl.WINDOWPOS_CENTERED, sdl.WINDOWPOS_CENTERED, 1024, 576, sdl.WINDOW_RESIZABLE)
+	if err != nil {
+		fmt.Printf("Could not create window: %s\n", sdl.GetError())
+	}
+	defer window.Destroy()
+
+	// Create renderer
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Printf("Could not create renderer: %s\n", sdl.GetError())
+	}
+	defer renderer.Destroy()
+
+	// Run game loop
+	for game.running {
+		getEvents(&game)
+		display(&game, renderer)
+	}
+}
+
+func getEvents(game *Game) {
+	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+		switch e := event.(type) {
+		case *sdl.QuitEvent:
+			game.running = false
+			break
+		case *sdl.KeyboardEvent:
+			if e.Keysym.Sym == sdl.K_ESCAPE{
+				game.running = false
+			} else if e.Keysym.Sym == sdl.K_SPACE{
+				if e.Type == 768 { // if keyboard event is keydown
+					game.colored = true
+				} else {
+					game.colored = false
+				}
+			}
+			break
+		}
+	}
+}
+
+func display(game *Game, renderer *sdl.Renderer) {
+	if game.colored {
+		renderer.SetDrawColor(161, 0, 255, 255)
+	} else {
+		renderer.SetDrawColor(211, 255, 142, 255)
+	}
+	renderer.Clear()
+	renderer.Present()
+}
